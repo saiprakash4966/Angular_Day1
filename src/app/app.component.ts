356d00a59e744dc746e933d46762e22a30f922ba
Your Name <you@example.com>
2022-11-07 11:49:36 +0530

    Added:Bridgelabz Site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hello_World';
   imageUrl="../assets/logo.jpg";
+  url ="https://www.bridgelabz.com";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
+    }
+    onClick($event: any){
+      console.log("Save button is clicked!", $event);
+      window.open(this.url, "_blank");
     }}
