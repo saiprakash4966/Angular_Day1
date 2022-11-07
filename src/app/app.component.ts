3216afb1d60325d55e640e65be7312b2844f7017
Your Name <you@example.com>
2022-11-07 12:01:01 +0530

    Added : Username validation using input even binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello_World';
-  imageUrl="../assets/logo.jpg";
-  url ="https://www.bridgelabz.com";
-  userName: string ="";
+  title = 'HelloWorld from BrideLabz';
+  imageUrl = "../assets/logo.jpg"
+  url = "https://www.bridgelabz.com/"
+  userName:string="";
+  nameError:string="";
 
   ngOnInit(): void {
-    this.title = "Hello from BridgeLabz.";
+    this.title ="Hello All"
+  }
+
+  onClick($event: any){
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
+  onInput($event:any)
+  {
+    console.log("change event occured!",$event.data);
+    const nameRegex=RegExp('[A-Z]{1}[a-z]{2}');
+    if(nameRegex.test(this.userName))
+    {
+      this.nameError="";
+      this.title=this.userName;
+      return;
     }
-    onClick($event: any){
-      console.log("Save button is clicked!", $event);
-      window.open(this.url, "_blank");
-    }}
+    this.nameError="Name Is Incorrect";
+}}
