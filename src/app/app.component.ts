4ef80eccc128b4c736f8ff914412d09d477b6494
Your Name <you@example.com>
2022-11-07 11:33:57 +0530

    Added:Hello from Bridgelabz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,29 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HelloWorld from BrideLabz';
-  imageUrl = "../assets/logo.jpg"
-  url = "https://www.bridgelabz.com/"
-  userName:string="";
-  nameError:string="";
+  title = 'Hello_World';
 
   ngOnInit(): void {
-    this.title ="Hello All"
-  }
-
-  onClick($event: any){
-    console.log("Save button is clicked!", $event);
-    window.open(this.url, "_blank");
-  }
-  onInput($event:any)
-  {
-    console.log("change event occured!",$event.data);
-    const nameRegex=RegExp('[A-Z]{1}[a-z]{2}');
-    if(nameRegex.test(this.userName))
-    {
-      this.nameError="";
-      this.title=this.userName;
-      return;
+    this.title = "Hello from BridgeLabz";
     }
-    this.nameError="Name Is Incorrect";
-}}
+}
